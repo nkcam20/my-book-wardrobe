fe2565a7acad3dee27e7891a70713187659b039b
S. Nanda Kumar <155619327+nkcam20@users.noreply.github.com>
2026-03-23 13:26:29 +0000

    final fixed deploy

--- vite.config.ts
+++ vite.config.ts
@@ -1,22 +1,7 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
-import path from "path";
-import { componentTagger } from "lovable-tagger";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react-swc'
 
-// https://vitejs.dev/config/
-export default defineConfig(({ mode }) => ({
-base: '/my-book-wardrobe/',
-  server: {
-    host: "::",
-    port: 8080,
-    hmr: {
-      overlay: false,
-    },
-  },
-  plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
-  },
-}));
+export default defineConfig({
+  base: '/my-book-wardrobe/',
+  plugins: [react()],
+})
